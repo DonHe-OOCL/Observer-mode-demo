6c015ef110570cf3cdbeb077771ce005b481bd75
HEDO <hedo@oocl.com>
2024-11-24 23:03:18 +0800

    test: add lottery test case

--- src/test/java/com/lottery/ApiTest.java
+++ src/test/java/com/lottery/ApiTest.java
@@ -1,5 +1,20 @@
 package com.lottery;
 
-public class ApiTest {
+import com.alibaba.fastjson.JSON;
+import com.lottery.entity.LotteryResult;
+import com.lottery.service.LotteryService;
+import com.lottery.service.LotteryServiceImpl;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class ApiTest {
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void show_result() {
+        LotteryService lotteryService = new LotteryServiceImpl();
+        LotteryResult result = lotteryService.draw("2765789109876");
+        logger.info("测试结果：{}", JSON.toJSONString(result));
+    }
 }
